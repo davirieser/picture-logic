5579951f00afb99319e96dd732d01c793c578bb9
davirieser <david.w.rieser@gmail.com>
2026-01-02 10:18:57 +0100

    added tailwind forms

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,13 +1,38 @@
 <script lang="ts">
-    import { PALETTE } from "$lib/storable";
+    import { ENHANCED_BORDER_SPACING, PALETTE } from "$lib/storable";
 	import { getPalleteClasses, PALETTES } from "$lib/util";
 </script>
 
-<label for="palette-select" class="block text-sm/6 font-medium">
-    Color Palette
-</label>
-<select bind:value={$PALETTE} id="palette-select">
-    {#each PALETTES as palette}
-        <option class={getPalleteClasses(palette, "bg", 200)}>{palette}</option>
-    {/each}
-</select>
+<div class="divide-y divide-white/5">
+    <label for="palette-select" class="block">
+        <span class="text-gray-700 dark:text-gray-300">Color Palette</span>
+        <select bind:value={$PALETTE} id="palette-select" class={{
+            block: true,
+            "w-full": true,
+            "rounded-md": true,
+            ...getPalleteClasses($PALETTE, "bg", 200),
+            ...getPalleteClasses($PALETTE, "border", 300),
+            "shadow-md": true,
+            "focus:ring": true,
+            "mt-1": true,
+        }}>
+            {#each PALETTES as palette}
+                <option class={getPalleteClasses(palette, "bg", 200)}>{palette}</option>
+            {/each}
+        </select>
+    </label>
+
+    <label for="grid-size" class="block">
+        <span class="text-gray-700 dark:text-gray-300">Grid Size</span>
+        <input type="number" id="grid-size" bind:value={$ENHANCED_BORDER_SPACING} class={{
+            block: true,
+            "w-full": true,
+            "rounded-md": true,
+            ...getPalleteClasses("neutral", "border", 100),
+            ...getPalleteClasses("neutral", "bg", 50),
+            "shadow-md": true,
+            "focus:ring": true,
+            "mt-1": true,
+        }} />
+    </label>
+</div>
